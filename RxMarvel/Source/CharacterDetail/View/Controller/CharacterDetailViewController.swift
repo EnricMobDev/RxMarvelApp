29cc513f3a66501388a32eaed824d9c04e4ed2dc
Enric Pou <epouvillanueva@gmail.com>
2019-11-11 18:35:48 +0100

    folder structure

---
+++ RxMarvel/Source/CharacterDetail/View/Controller/CharacterDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDetailViewController.swift
+//  RxMarvel
+//
+//  Created by Enric Pou Villanueva on 11/11/2019.
+//  Copyright © 2019 Enric Pou Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
